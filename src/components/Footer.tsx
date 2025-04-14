2d83db430064b1abebc3660a845ef69617560c83
Ahmednaser1012 <Ahmed_201900108@fci.helwan.edAhu.eg>
2025-04-14 20:50:27 +0200

    Final Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,8 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="pt-16 mt-16  border-t border-gray-800">
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-11">
-        {/* العمود الأول */}
+    <footer className="pt-12 mt-12  border-t border-gray-800">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-14">
         <div className="mb-5 md:mb-0 text-center md:text-left">
           <h2 className="font-chillax font-medium text-2xl text-text_default mb-5">
             nique.
@@ -17,10 +16,8 @@ export const Footer = () => {
           </p>
         </div>
 
-        {/* العمود الثاني والثالث جنب بعض ومتمركزين */}
         <div className="w-full px-4 flex justify-center md:block col-span-1 md:col-span-2">
           <div className="grid grid-cols-2 gap-8 w-full max-w-md text-center md:text-left">
-            {/* العمود الثاني */}
             <div>
               <h5 className="font-chillax font-normal text-base mb-5">Pages</h5>
               <ul className="space-y-5 font-chillax font-normal text-base text-text_muted ">
@@ -48,7 +45,6 @@ export const Footer = () => {
               </ul>
             </div>
 
-            {/* العمود الثالث */}
             <div>
               <h5 className="font-chillax font-normal text-base mb-5">
                 Utility Pages
