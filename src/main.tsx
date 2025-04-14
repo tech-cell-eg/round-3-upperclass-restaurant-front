239c4056c86f9d26277083684709fc76b0ce656b
moelsayed011 <me6758478@gmail.com>
2025-04-14 10:31:31 +0200

    feat:add main style of the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-
 import { PrimeReactProvider } from "primereact/api";
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <PrimeReactProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,38 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bitter: ["Bitter Rose"],
+        chillax: ["Chillax"],
+      },
+      colors: {
+        // Define your colors for text
+        text_default: "#FFFFFF",
+        text_primary: "#FACE8D",
+        text_inverse: "#081212", // navbar
+        text_muted: "#CCCCCC",
+
+        // Define your colors for background
+        bg_default: "#081212",
+        bg_primary: "#FACE8D",
+
+        // Define your colors for border
+        border_default: "#FFFFFF1A",
+
+        // Define your colors for icons
+        icon_default: "#FFFFFF",
+        icon_primary: "#FACE8D",
+        icon_inverse: "#081212",
+      },
+
+      borderRadius: {
+        image: "10px",
+        nav: "100px", // iclude Button in nav
+        button: "500px",
+        default: "16px",  // Default border div in menu section
+      },
+    },
   },
   plugins: [],
 };
